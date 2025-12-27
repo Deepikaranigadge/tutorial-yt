8435438e241ed8bb7e6986f3424bd3c6bd1da89e
Deepikaranigadge <deepam.sabhuo1@gmail.com>
2025-12-28 01:05:53 +0530

    GetPost Practices

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
-app = Flask(__name__) 
+app = Flask(__name__)
+
+users = []  # fake database
 
 @app.route("/", methods=["GET", "POST"])
-def home():
+def users_page():
     if request.method == "POST":
         name = request.form.get("name")
-        return f"Hello, {name}"
+        email = request.form.get("email")
 
-    return render_template("index.html") 
+        if name and email:
+            users.append({
+                "id": len(users) + 1,
+                "name": name,
+                "email": email
+            })
+
+        return redirect(url_for("users_page"))
+
+    return render_template("users.html", users=users)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
